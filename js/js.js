9132a3b9d86223e5809b4f1db62dc3af13cd70f8
KOKOLIKO SERGUEÏ <bradfalconnek@gmail.com>
2023-11-02 13:18:34 +0000

    cheickpointAlgo

---
+++ js/js.js
@@ -0,0 +1,13 @@
+ 
+ 
+ function somme(nombre1, nombre2, nombre3){
+    var total = nombre1 + nombre2 + nombre3
+
+    console.log("la somme de nombre1, nombre2 et nombre3 est: ",
+    total);
+
+    console.log(`la somme de ${nombre1}, ${nombre2} et ${nombre3} est: `, total);
+    
+
+}
+somme(5,7,3);
